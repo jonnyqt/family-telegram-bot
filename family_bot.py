050a77d84661cb2510e60c6f33e2f9022206f27f
jonnyqt <johnjonnyqt@gmail.com>
2026-01-11 13:33:51 +0900

    Update family_bot.py

--- family_bot.py
+++ family_bot.py
@@ -1,14 +1,14 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-# Обработчик команды /start
+# Асинхронная функция-обработчик
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Привет! Я твой семейный бот!")
 
-# Создаем приложение
-app = ApplicationBuilder().token("ВАШ_ТОКЕН_ТЕЛЕГРАМ").build()
+# Создаем приложение бота
+app = ApplicationBuilder().token("ВАШ_ТОКЕН").build()
 
-# Регистрируем обработчик
+# Регистрируем обработчик команды /start
 app.add_handler(CommandHandler("start", start))
 
 # Запуск бота
